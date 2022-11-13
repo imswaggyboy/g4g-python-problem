d2ca0070de057be4bed7005d3aa780f04c86c695
Surya Murugan <75663923+imswaggyboy@users.noreply.github.com>
2022-11-13 19:31:13 +0530

    Update second_largest.py

--- second_largest.py
+++ second_largest.py
@@ -1,15 +1,17 @@
 def print2largest(self,arr, n):
-		# code here
-		arr.sort()
-		mx = arr[-1]
-        for i in range(1,len(arr)):
-            if arr[i] > mx:
-                mx = arr[i]
-        s_h = int()
-        for i in range(1,n+1):
-            if arr[-i]<mx:
-                
-                s_h = arr[-i]
-                break
-            else: s_h = -1
-        return s_h
+	# code here
+	#lets sort the array to get the highest value
+	arr.sort()
+	#lets assume that in array the last element will be the highest element
+	mx = arr[-1]
+# 	initializing the s_h (second heighest) as int() with no value becoz in future we are going to update it
+	s_h = int()
+# 	using loop so that to check the element,using index we will if that particular index element is lesser than mx value
+	for i in range(1,n+1):
+# 		if yes the update the s_h and break the loop
+	    if arr[-i]<mx:
+		s_h = arr[-i]
+		break
+# 		else print -1 as no second highest present in the array 
+	    else: s_h = -1
+	return s_h
