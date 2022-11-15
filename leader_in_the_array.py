411d96bdb185e26ace56bffd30c8e0fa8a11844b
Surya Murugan <75663923+imswaggyboy@users.noreply.github.com>
2022-11-15 19:09:36 +0530

    Update leader_in_the_array.py

--- leader_in_the_array.py
+++ leader_in_the_array.py
@@ -16,3 +16,24 @@ def leaders(self, A, N):
         l.reverse()
 
         return l
+
+
+
+# leader = []
+        # for i in range(N):
+        #     vote = 0
+        #     for j in range(i+1,N):
+        #         if A[i]> A[j]:
+        #             vote = vote+1
+        #     if (len(A)-(i+1)) == vote:
+        #         leader.append(A[i])
+        # return leader
+_______________________________________________________
+        # n_l = [x for x in A]
+        # leader = []
+        # while len(A) >0:
+        #     if A[0] ==max(A):
+        #         leader.append(A[0])
+        #         A.remove(A[0])
+        #     else: A.remove(A[0])
+        # return leader
